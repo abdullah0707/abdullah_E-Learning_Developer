246d69c8287b7e702cf4d3a7a1d6b2688f96eee3
abdullah0707 <abdullahelgammal25@gmail.com>
2024-10-24 23:44:47 +0300

    up 9

--- assets/js/code.js
+++ assets/js/code.js
@@ -58,6 +58,16 @@
    });
 
 
+   //Travel portfolio page
+
+   $(".travel").on('click', function () {
+      $("body").addClass("travel-on");
+   });
+   $(".travel-close").on('click', function () {
+      $("body").removeClass("travel-on");
+   });
+
+
    //Wildlife portfolio page
 
    $(".wildlife").on('click', function () {
@@ -66,6 +76,17 @@
    $(".wildlife-close").on('click', function () {
       $("body").removeClass("wildlife-on");
    });
+
+
+   //Nature portfolio page
+
+   $(".nature").on('click', function () {
+      $("body").addClass("nature-on");
+   });
+   $(".nature-close").on('click', function () {
+      $("body").removeClass("nature-on");
+   });
+
 
 })(jQuery);
 
